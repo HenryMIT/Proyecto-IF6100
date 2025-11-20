a59a7c71e0a03c0470dfc74a58f83daaf6f82da0
KillerMen21 <162850183+KillerMen21@users.noreply.github.com>
2025-11-20 17:03:29 -0600

    Avance Jeff

--- Frontend/src/app/app.config.ts
+++ Frontend/src/app/app.config.ts
@@ -4,12 +4,19 @@ import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { jwtInterceptor } from './shared/helpers/interceptors/jwt-interceptor';
 import { refreshInterceptor } from './shared/helpers/interceptors/refresh-interceptor';
+import localeEsCR from '@angular/common/locales/es-CR';
+import { registerLocaleData } from '@angular/common';
+import { LOCALE_ID } from '@angular/core';
+
+registerLocaleData(localeEsCR);
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
     provideRouter(routes),
-    provideHttpClient(withInterceptors([jwtInterceptor, refreshInterceptor]))    
+    provideHttpClient(withInterceptors([jwtInterceptor, refreshInterceptor])),
+
+    { provide: LOCALE_ID, useValue: 'es-CR' }
   ]
 };
